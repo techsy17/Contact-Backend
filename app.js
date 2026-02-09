6f0e56bb568374515ae35eca4071dd8638660c87
techsy17 <tech.sy17@gmail.com>
2026-02-09 18:45:12 +0530

    add url access

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.use(cors({
   ]
 }));
 
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests like Postman / server-to-server (no origin)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `CORS policy: This origin is not allowed -> ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 app.use(express.json());
 app.use(contactRoute);
